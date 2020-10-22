d342ddf0b117237ca757cc9400a79b818104fea7
Jailson Xavier <xavier.jailson@outlook.com>
2020-10-22 20:09:27 -0300

    Exercicio de Funcionarios

---
+++ Funcionarios/Funcionarios.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace Funcionarios
+{
+    class Funcionarios
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public double Salario { get; private set; }
+
+        public Funcionarios(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void AumentoDeSalario(double porcentagem)
+        {
+            Salario += Salario * porcentagem / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return "Id: " + Id + "," + "Nome: " + Nome + " , " + "Salário R$: " + Salario;
+        }
+
+    }
+}
---
+++ Funcionarios/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Funcionarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos Funcionários serão registrados ?");
+            int n = int.Parse(Console.ReadLine());
+            List<Funcionarios> list = new List<Funcionarios>();
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Funcionarios: " + "#" + i);
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salário: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Funcionarios(id, nome, salario));
+            }
+            Console.Write("Digite o Id do Funcionário que terá aumento de salário: ");
+            int id1 = int.Parse(Console.ReadLine());
+
+            Funcionarios fun = list.Find(x=> x.Id == id1);
+            if (fun != null)
+            {
+                Console.Write("Digite a porcentagem: ");
+                double porcentagem = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                fun.AumentoDeSalario(porcentagem);
+               
+            }
+            else
+            {
+                Console.WriteLine("Id não existe!");
+            }
+
+            Console.WriteLine("Lista de funcionários atualizada: ");
+            foreach (var lista in list)
+            {
+                Console.WriteLine(lista);
+            }
+           
+
+        }
+
+    }
+}
